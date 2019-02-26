48d878afa96846ab9a4de99256f008d39a97698a
AndrasSzemes <andras@szemes.eu>
2019-02-26 13:55:09 +0100

    Refactor Meteor and Spaceship class

--- Meteor.java
+++ Meteor.java
@@ -1,9 +1,8 @@
 public class Meteor {
-  private String ascii;
+  private final String ASCII = "MMM8&&&MMMM88&&&MMMM88&&&&&MMMM88&&&MMM8&&&";
   private int[] origo;
-	
+
 	Meteor() {
-		ascii = "MMM8&&&MMMM88&&&MMMM88&&&&&MMMM88&&&MMM8&&&";
 		origo = new int[] {15, 90};
 	}
 
@@ -13,9 +12,10 @@ public class Meteor {
   }
 	public int getOrigoY() {return origo[0];}
   public int getOrigoX() {return origo[1];}
+
   public int[][] getCoord() {
 		int[][] coordinates = {
-			{origo[0]-2, origo[1]-3}, {origo[0]-2, origo[1]-2}, {origo[0]-2, origo[1]-1}, {origo[0]-2, origo[1]}, {origo[0]-2, origo[1]+1}, 
+			{origo[0]-2, origo[1]-3}, {origo[0]-2, origo[1]-2}, {origo[0]-2, origo[1]-1}, {origo[0]-2, origo[1]}, {origo[0]-2, origo[1]+1},
 			{origo[0]-2, origo[1]+2}, {origo[0]-2, origo[1]+3},
 			{origo[0]-1, origo[1]-4}, {origo[0]-1, origo[1]-3}, {origo[0]-1, origo[1]-2}, {origo[0]-1, origo[1]-1}, {origo[0]-1, origo[1]},
 			{origo[0]-1, origo[1]+1}, {origo[0]-1, origo[1]+2}, {origo[0]-1, origo[1]+3}, {origo[0]-1, origo[1]+4},
@@ -24,10 +24,10 @@ public class Meteor {
 			{origo[0], origo[1]+5},
 			{origo[0]+1, origo[1]-4}, {origo[0]+1, origo[1]-3}, {origo[0]+1, origo[1]-2}, {origo[0]+1, origo[1]-1}, {origo[0]+1, origo[1]},
 			{origo[0]+1, origo[1]+1}, {origo[0]+1, origo[1]+2}, {origo[0]+1, origo[1]+3}, {origo[0]+1, origo[1]+4},
-			{origo[0]+2, origo[1]-3}, {origo[0]+2, origo[1]-2}, {origo[0]+2, origo[1]-1}, {origo[0]+2, origo[1]}, {origo[0]+2, origo[1]+1}, 
+			{origo[0]+2, origo[1]-3}, {origo[0]+2, origo[1]-2}, {origo[0]+2, origo[1]-1}, {origo[0]+2, origo[1]}, {origo[0]+2, origo[1]+1},
 			{origo[0]+2, origo[1]+2}, {origo[0]+2, origo[1]+3}
 		};
 		return coordinates;
 	}
-  public String getAscii() {return ascii;}
+  public String getAscii() {return ASCII;}
 }
